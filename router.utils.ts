fb41ea94052b4911c424311acc79a3f793ad8eb4
David Colon <dcolonantonio@gmail.com>
2021-10-09 23:16:39 -0400

    clean up router

--- router.utils.ts
+++ router.utils.ts
@@ -3,21 +3,25 @@ import express from "express";
 import * as WalletController from "./wallet/wallet.controller";
 
 export function init(app: express.Application) {
-  // todo figure out how to seperate this
+  app.use("/", rootRouter());
+  app.use("/interactions", interactionsRouter());
+}
+
+function rootRouter() {
   const router = express.Router();
   router.get("/ping", (req: express.Request, res: express.Response) => {
     console.log(chalk.green("succesful ping"));
-    return res.sendStatus(200).json({ success: "🚀 ~ Ping Successful" });
+    return res.json({ success: "🚀 ~ Ping Successful" });
   });
   router.get("/", (req: express.Request, res: express.Response) => {
-    return res.sendStatus(200).json({ success: "ok!" });;
+    return res.json({ success: "ok!" });;
   });
-  
-  const interactionsRouter = express.Router();
-  interactionsRouter.get("/wallet/:address", WalletController.interactions);
-  // app.get('/contract/:address', ContractController.interactions)
-  app.use("/", router);
-
-  app.use("/interactions", interactionsRouter);
+  return router;
 }
 
+function interactionsRouter() {
+  const router = express.Router()
+  router.get("/wallet/:address", WalletController.interactions);
+  // app.get('/contract/:address', ContractController.interactions)
+  return router
+}
