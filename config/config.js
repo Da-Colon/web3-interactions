418089bb9c53b2de518225ec51abe7c96bf1c1eb
David Colon <dcolonantonio@gmail.com>
2021-10-10 16:29:17 -0400

    give different names to databases

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     "test": {
       "username": process.env.POSTGRES_USER || "postgres",
       "password": process.env.POSTGRES_PASSWORD,
-      "database": process.env.POSTGRES_DB,
+      "database": 'web3-interactions-test',
       "host": process.env.POSTGRES_HOST || "localhost",
       "port": process.env.POSTGRES_PORT || "5432",
       "dialect": "postgres",
@@ -20,7 +20,7 @@ module.exports = {
     "production": {
       "username": process.env.POSTGRES_USER || "postgres",
       "password": process.env.POSTGRES_PASSWORD,
-      "database": process.env.POSTGRES_DB,
+      "database": web3-interactions-prod,
       "host": process.env.POSTGRES_HOST || "localhost",
       "port": process.env.POSTGRES_PORT || "5432",
       "dialect": "postgres",
